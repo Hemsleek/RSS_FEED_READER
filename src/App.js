b0af7c2b1030389b5d69bb92cd7a1a43a37786d0
hemsleek <asiyanbimubashir@gmail.com>
2021-05-11 22:57:00 +0100

    feat: user form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     rss feed reader
+      <form>
+        <div className="form-group">
+          <label>URL</label>
+          <input type="text" placeholder="Enter Url" />
+        </div>
+        <button>Add</button>
+      </form>
+
     </div>
   );
 }
